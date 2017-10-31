f0206fa3a6a18f29bb70c25461f1215ffc28c1d3
jhinchley <jhinchley@tamu.edu>
2017-10-31 14:30:08 -0500

    feat: added simple multithreaded function using  Tasks

--- LearningDotNet/Program.cs
+++ LearningDotNet/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LearningDotNet
@@ -10,6 +11,19 @@ namespace LearningDotNet
     {
         static void Main(string[] args)
         {
+            Console.Write("Downloading file....");
+            Download();
+            Console.ReadLine();
+        }
+
+        private static void Download()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Download complete");
+            });
+            
         }
     }
 }
