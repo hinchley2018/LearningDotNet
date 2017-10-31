8e35627ffc45e8e5fc0c0491ae49297a8b45ccf6
jhinchley <jhinchley@tamu.edu>
2017-10-31 14:56:04 -0500

    refactor: moved network download to separate class

--- LearningDotNet/Program.cs
+++ LearningDotNet/Program.cs
@@ -1,29 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
 namespace LearningDotNet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Downloading file....");
-            Download();
+
+            Network.Download("PizzaImage");
+            Network.Download("FoxImage");
             Console.ReadLine();
         }
 
-        private static void Download()
-        {
-            Task.Run(() =>
-            {
-                Thread.Sleep(3000);
-                Console.WriteLine("Download complete");
-            });
-            
-        }
+       
     }
 }
---
+++ Network.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace LearningDotNet
+{
+    class Network
+    {
+        public Network()
+        {
+
+        }
+        static public Task Download(String fn)
+        {
+            return Task.Run(() => {
+                Console.Write("Downloading {0}....", fn);
+                Thread.Sleep(3000);
+                Console.WriteLine("Download complete");
+            }
+            );
+        }
+    }
+}
+
